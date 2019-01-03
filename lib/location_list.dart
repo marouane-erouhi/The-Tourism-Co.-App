8d8747da6f437b9d550339751fd0c00f8186fc05
Marouane ER-Rouhi <marouane.erouhi@gmail.com>
2019-01-02 20:58:45 -0500

    first commit

---
+++ lib/location_list.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'models/location.dart';
+import 'styles.dart';
+import 'location_detail.dart';
+
+class LocationList extends StatelessWidget{
+  final List<Location> _locations;
+
+  LocationList(this._locations);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Locations",
+          style: Styles.navBarTitle
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: _locations.length,
+        itemBuilder: _listViewItemBuilder,
+      )
+    );
+  }
+
+  Widget _listViewItemBuilder(BuildContext context, int index){
+    print("_listViewItemBuilder run");
+    Location location = _locations[index];
+    return ListTile(
+      leading: _itemThumbnail(location),
+      title: _itemTitle(location),
+      contentPadding: EdgeInsets.all(10.0),
+      onTap: (){
+        _navigateToLocationDetail(context,location);
+      },
+    );
+  }
+
+  void _navigateToLocationDetail(BuildContext context, Location location){
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => LocationDetail(location),
+        ));
+  }
+
+  Widget _itemThumbnail(Location location){
+    return Container(
+      constraints: BoxConstraints(
+        maxWidth: 100.0
+      ),
+      child: Image.network(location.url, fit:BoxFit.fitHeight),
+    );
+  }
+  Widget _itemTitle(Location location){
+    return Text(location.name, style: Styles.textDefault);
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'models/location.dart';
+import 'models/mocks/mock_location.dart';
+
+import 'location_list.dart';
+
+void main() {
+  final List<Location> mockLocations = MockLocation.FetchAll();
+
+  return runApp(MaterialApp(
+    home: LocationList(mockLocations)
+  ));
+}

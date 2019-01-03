54cf2fe217772d177363c98d19ca7acfc8c29c8f
Marouane ER-Rouhi <marouane.erouhi@gmail.com>
2019-01-02 22:40:33 -0500

    added unit test for the MockLocation class

---
+++ test/unit/mock_location_test.dart
@@ -0,0 +1,22 @@
+import 'package:test_api/test_api.dart';
+import '../../lib/models/mocks/mock_location.dart';
+
+void main() {
+  test('test fetchAny', (){
+    final mockLocation = MockLocation.fetchAny();
+    expect(mockLocation, isNotNull);
+    expect(mockLocation.name, isNotEmpty);
+  });
+
+  test('test fetchAll', (){
+    final mockLocation = MockLocation.fetchAll();
+    expect(mockLocation.length, greaterThan(0));
+    expect(mockLocation[0].name, isNotEmpty);
+  });
+  test('test fetch', (){
+    final mockLocation = MockLocation.fetch(0);
+    expect(mockLocation, isNotNull);
+    expect(mockLocation.name, isNotEmpty);
+  });
+  
+}

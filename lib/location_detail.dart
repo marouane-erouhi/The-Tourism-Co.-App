7bf409a60dcc30e3fd35366dc0ab980761a429e9
Marouane ER-Rouhi <marouane.erouhi@gmail.com>
2019-01-03 21:41:24 -0500

    redesign of LocationDetail page

--- lib/location_detail.dart
+++ lib/location_detail.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'models/location.dart';
+import 'components/location_tile.dart';
 import 'styles.dart';
-import 'models/mocks/mock_location.dart';
+
+const BannerImageHeight = 300.0;
+const BodyVerticalPadding = 20.0;
+const FooterHeight = 100.0;
 
 class LocationDetail extends StatefulWidget {
   final int locationID;
@@ -34,7 +39,7 @@ class _LocationDetailState extends State<LocationDetail> {
   }
 
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -42,21 +47,69 @@ class _LocationDetailState extends State<LocationDetail> {
           style: Styles.navBarTitle
         ),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: _renderBody(context,location)
-        ),
+      body: Stack(
+        children:[
+          _renderBody(context,location),
+          _renderFooter(context, location)
+        ],
       )
     );
   }
 
-  List<Widget> _renderBody(BuildContext context, Location location){
+  Widget _renderBody(BuildContext context, Location location){
     var result = List<Widget>();
-    result.add(_bannerImage(location.url, 170.0));
+    result.add(_bannerImage(location.url, BannerImageHeight));
+    result.add(_renderHeader());
     result.addAll(_renderFacts(context, location));
-    return result;
+    return SingleChildScrollView(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: result
+        ),
+    );
+  }
+
+  Widget _renderFooter(BuildContext context, Location location){
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(color: Colors.white.withOpacity(0.7)),
+          height: FooterHeight,
+          child: Container(
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal:30.0),
+            child: _renderBookButton(),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _renderBookButton(){
+    return FlatButton(
+      color: Styles.accentColor,
+      textColor: Styles.textColorBright,
+      onPressed: () => _handleBookPress,
+      child: Text('Book'.toUpperCase(), style: Styles.textCTAButton),
+    );
+  }
+
+  _handleBookPress() async{
+    const url = 'mailot:hello@tourism.co?subject=inquiry';
+    if(await canLaunch(url)){
+      await launch(url);
+    }else{
+      throw 'Could not  launch $url';
+    }
+  } 
+
+  Widget _renderHeader(){
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: BodyVerticalPadding, horizontal: Styles.horizontalPaddingDefault),
+      child: LocationTile(location: this.location, darkTheme: false,),
+    );
   }
 
   List<Widget> _renderFacts(BuildContext context, Location location){
@@ -69,9 +122,9 @@ class _LocationDetailState extends State<LocationDetail> {
   }
   Widget _sectionTitle(String text){
     return Container(
-      padding: EdgeInsets.fromLTRB(25.0, 25.0, 25.0, 10.0),
+      padding: EdgeInsets.fromLTRB(Styles.horizontalPaddingDefault, 25.0, Styles.horizontalPaddingDefault, 0.0),
       child: Text(
-        text,
+        text.toUpperCase(),
         textAlign: TextAlign.left,
         style: Styles.headerLarge
       )
@@ -79,7 +132,7 @@ class _LocationDetailState extends State<LocationDetail> {
   }
   Widget _sectionText(String text){
     return Container(
-      padding: EdgeInsets.fromLTRB(25.0, 15.0, 25.0, 15.0),
+      padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: Styles.horizontalPaddingDefault),
       child: Text(
         text,
         style: Styles.textDefault,
